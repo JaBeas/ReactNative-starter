78929a183cc732cebaa40998217220660a8f5546
Jaikar Beasley <jaikarbeasley@gmail.com>
2021-08-03 19:11:45 -0700

    added reducer and updated formatting

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -4,9 +4,9 @@ import { Text, View, StyleSheet, Button } from 'react-native';
 function ColorCounter( { color, onIncrease, onDecrease } ) {
   return (
    <View>
-    <Text>{color}</Text>
-    <Button onPress={() => onIncrease()} title={`Increse ${color} `}/>
-    <Button onPress={() => onDecrease()}  title={`Decrese ${color} `}/>
+      <Text>{color}</Text>
+      <Button onPress={() => onIncrease()} title={`Increse ${color} `}/>
+      <Button onPress={() => onDecrease()}  title={`Decrese ${color} `}/>
    </View>
   );
 }
